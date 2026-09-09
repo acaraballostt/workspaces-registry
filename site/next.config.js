aede8bf18d3f229fb3bd07cc90b6287617a763d1
acaraballostt <acaraballostt@gmail.com>
2026-09-09 02:45:57 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,15 +2,15 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
-    icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    nname: 'AC-Workspaces-Reg',
+    description: 'Specialized List of Apps for Workspaces.',
+    icon: 'workspaces.png',
+    listUrl: 'https://github.com/acaraballostt/workspaces-registry/',
+    contactUrl: 'https://github.com/acaraballostt/',
   },
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/kasm-registry/1.0',
+  basePath: '/workspaces-registry/1.0',
   trailingSlash: true,
   images: {
     unoptimized: true,
