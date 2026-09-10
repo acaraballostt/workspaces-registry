e07f84c71034e07735ebef93836f50b231cf0ac3
acaraballostt <acaraballostt@gmail.com>
2026-09-10 15:44:22 -0500

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     nname: 'AC-Workspaces-Reg',
     description: 'Specialized List of Apps for Workspaces.',
-    icon: 'https://github.com/acaraballostt/workspaces-registry/1.0/site/workspaces.png',
+    icon: 'https://github.com/acaraballostt/workspaces-registry/1.0/workspaces.png',
     listUrl: 'https://github.com/acaraballostt/workspaces-registry/',
     contactUrl: 'https://github.com/acaraballostt/',
   },
